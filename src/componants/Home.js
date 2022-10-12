532f40f032109bc9612b2f83459671bf24e2a2d6
ayakub <ayakub1086@gmail.com>
2022-10-12 20:58:28 +0600

    ready project

--- src/componants/Home.js
+++ src/componants/Home.js
@@ -6,12 +6,12 @@ const Home = () => {
     const items = useLoaderData();
     return (
         <div>
-            <div className='mr-20 ml-20 bg-slate-700 px-10 py-7 mt-5 rounded-md md:flex '>
+            <div className='md:mr-10 md:ml-10 bg-slate-700 px-10 py-7 mt-5 rounded-md md:flex '>
                 <div className='md:w-50'>
-                    <img className='w-/4 rounded-md' src={pic} alt="" />
+                    <img className='w-4/4 rounded-md sm:content-center' src={pic} alt="" />
                 </div>
                 <div className='text-gray-300 w-50 ml-3'>
-                    <h1 className='text-2xl'> <p className='md:none lg:block'>quiz is a form of game or mind sport in which players attempt to answer questions correctly about a certain or variety of </p> subjects. Quizzes can be used as a brief assessment in education and similar fields to measure growth in knowledge, abilities, or skills</h1>
+                    <h1 className='text-2xl'>quiz is a form of game or mind sport in which players attempt to answer questions correctly about a certain or variety ofsubjects. <span className='sm:hidden'>Quizzes can be used as a brief assessment in education and similar fields to measure growth in knowledge, abilities, or skills</span></h1>
                 </div>
             </div>
             <div className=" bg-green-200 mt-5 ">
